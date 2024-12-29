72477b86ca863ddd2f3e6baa9f4149a6f4e7e5ca
Oleh <oleh.levch3nko@gmail.com>
2024-12-29 19:35:58 +0200

    Revert "Initial version"
    This reverts commit d795fc4171ba1fb26b433dd7327a72e6f565f796.

--- server/prisma/migrations/20241227125004_add_ondelete_constraint/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Attachment" DROP CONSTRAINT "Attachment_messageId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "Message"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatGateway } from './chat/chat.gateway';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import { UsersService } from './users/users.service';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule,
-    UsersModule,
-    PrismaModule,
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, UsersService, ChatGateway, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
